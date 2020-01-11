37f6ead840874127783c53235510486e6fa855b2
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-01-10 16:34:30 -0800

    got prompts working

--- app.js
+++ app.js
@@ -8,68 +8,108 @@ const readFileAsync = util.promisify(fs.readFile);
 
 inquirer.registerPrompt("recursive", require("inquirer-recursive"));
 
-promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "managersName",
-      message: "What is your managers name?"
-    },
-    {
-      type: "input",
-      name: "managersId",
-      message: "What is your managers ID?"
-    },
-    {
-      type: "input",
-      name: "managersEmail",
-      message: "What is your managers Email?"
-    },
-    {
-      type: "input",
-      name: "managersOfficeNumber",
-      message: "What is your managers office number?"
-    },
-    {
-      type: "input",
-      name: "selection",
-      message: "What type of team member would you like to add?",
-      choices: [
-        "Engineer",
-        "Intern",
-        "I don't want to add any more team members"
-      ]
-    }
-    // {
-    //   type: "input",
-    //   name: "engineersName",
-    //   message: "What is your engineer's name?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "engineersId",
-    //   message: "What is your engineers ID?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "engineersGithub",
-    //   message: "What is your engineers Github?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "internsName",
-    //   message: "What is your intern's name?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "internsEmail",
-    //   message: "What is your intern's email?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "internsSchool",
-    //   message: "What is your intern's school?"
-    // }
-  ]);
+promptManager = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managersName",
+        message: "What is your managers name?"
+      },
+      {
+        type: "input",
+        name: "managersId",
+        message: "What is your managers ID?"
+      },
+      {
+        type: "input",
+        name: "managersEmail",
+        message: "What is your managers Email?"
+      },
+      {
+        type: "input",
+        name: "managersOfficeNumber",
+        message: "What is your managers office number?"
+      }
+    ])
+    .then(function() {
+      askForEmployee();
+    });
 };
-promptUser();
+
+askForEmployee = function() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selection",
+        message: "What type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ])
+    .then(val => {
+      if (val.selection === "Engineer") {
+        engineerQuestions();
+      } else if (val.selection === "Intern") {
+        internQuestions();
+      } else if (
+        val.selection === "I don't want to add any more team members"
+      ) {
+        generatehtml();
+      }
+    });
+};
+
+internQuestions = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internsName",
+        message: "What is your intern's name?"
+      },
+      {
+        type: "input",
+        name: "internsEmail",
+        message: "What is your intern's email?"
+      },
+      {
+        type: "input",
+        name: "internsSchool",
+        message: "What is your intern's school?"
+      }
+    ])
+    .then(internAnswers => {
+      askForEmployee();
+    });
+};
+
+engineerQuestions = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineersName",
+        message: "What is your engineer's name?"
+      },
+      {
+        type: "input",
+        name: "engineersId",
+        message: "What is your engineers ID?"
+      },
+      {
+        type: "input",
+        name: "engineersGithub",
+        message: "What is your engineers Github?"
+      }
+    ])
+    .then(enginnerAnswers => {
+      askForEmployee();
+    });
+};
+
+promptManager();
